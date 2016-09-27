bddac9860b2acacfa6cd09edb39024ec8ec0fd54
zjalleman <zjalleman@gmail.com>
2016-09-27 19:20:07 -0400

    a

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
-app.use('/', hike);
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
