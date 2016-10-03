f714a563c5701cf688c9f8421ed3ab0426ee0b46
zjalleman <zjalleman@gmail.com>
2016-10-03 15:53:35 -0400

    a

--- public/javascripts/getData.js
+++ public/javascripts/getData.js
@@ -2,22 +2,22 @@
 	"use strict";
 	
 	var main = function() {
-		var url = 'http://www3.unca.edu/schedules/dev/schedules-json.php?term=201660&department=CSCI';
+		var url = 'http://www3.unca.edu/schedules/dev/schedules-json.php?term=201660&department=CSCI&callback=?';
 		$.getJSON(url, function(sunResponse) {
 			console.log(sunResponse);
-			var $sunTable = $('<table>');
+			//var $sunTable = $('<table>');
 			var response = JSON.parse(sunResponse);
             console.log(response);
-			for (var prop in response) {
+			/*for (var prop in response) {
 				var $item = $('<tr>');
 				var $itemProp = $('<td>').text(prop);
 				var $itemVal = $('<td>').text(response[prop]);
 				$item.append($itemProp);
 				$item.append($itemVal);
 				$sunTable.append($item);	
-			}
+			}*/
 			
-			$('main').append($sunTable);
+			//$('main').append($sunTable);
 		});
 	};
 	
