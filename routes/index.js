e304cd6b7cce87107d6e5d2e401c6b1767752431
zjalleman <zjalleman@gmail.com>
2016-10-26 17:09:26 -0400

    Most of list added to db. still need to fix some things.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var options = {
     hostname: 'www3.unca.edu',
     port: 80, 
-    path: '/schedules/dev/schedules-json.php?term=201660&department=CSCI'
+    path: '/schedules/dev/schedules-json.php?term=201660&department=ALL'
 };
 
 var connection = mysql.createConnection({
@@ -36,17 +36,28 @@ var req = http.request(options, (res) => {
         body.push(chunk);
     });
     res.on('end', function() {
-        /*body = Buffer.concat(body).toString();
+        body = Buffer.concat(body).toString();
         console.log(JSON.parse(body).length);
         var i;
         for (i = 0; i < JSON.parse(body).length; i++) {
             var qBody = JSON.parse(body)[i];
             
-            connection.query('INSERT INTO `CourseInfo` (codeCourses,term,titleCourses,crn,hours,days,startTime,endTime,location,lmt,enr,wlCap,wlAct) VALUES("' + qBody.Code + '","' + qBody["Term Portion"] + '","' + qBody.Title + '",' + parseInt(qBody.CRN) + ',' + qBody["Maximum credit hours"] + ',"' + qBody.Days + '","' + qBody["Start time"] + '","' + qBody["End time"] + '","' + qBody.Location + '",' + qBody["Enrollment limit"] + ',' + qBody["Current enrollment"] + ',' + qBody["Waitlist total seats"] + ',' + qBody["Waitlist filled seats"] + ');', function(err, result) {
+            connection.query('INSERT IGNORE INTO `CourseInfo` (codeCourses,term,titleCourses,crn,hours,days,startTime,endTime,location,lmt,enr,wlCap,wlAct) VALUES("' + qBody.Code + '","' + qBody["Term Portion"] + '","' + connection.escape(qBody.Title) + '",' + parseInt(qBody.CRN) + ',' + qBody["Maximum credit hours"] + ',"' + qBody.Days + '","' + qBody["Start time"] + '","' + qBody["End time"] + '","' + qBody.Location + '",' + qBody["Enrollment limit"] + ',' + qBody["Current enrollment"] + ',' + qBody["Waitlist total seats"] + ',' + qBody["Waitlist filled seats"] + ');', function(err, result) {
+                if (err) {
+                    console.log(err);
+                    if (err.errno == 1064) {
+                        console.log("adfasdf");
+                    }
+                    throw err;
+                } 
+                //console.log(result.affectedRows);
+            });
+            connection.query('INSERT IGNORE INTO `Instructors` (idInstructors, nameInstructors) VALUES(' + i + ', "' + qBody["Instructor(s)"] + '");', function(err, result) {
                 if (err) throw err;
                 //console.log(result.affectedRows);
             });
-        }*/
+            console.log(i);
+        }
         
         //console.log(qBody.Code);
         
@@ -61,11 +72,13 @@ for (i = 0; i < csciArray.length; i++) {
 }*/
         
         connection.query('SELECT * FROM `CourseInfo`', function(err, rows, fields) {
+            console.log("hi");
             if (err) throw err;
             var text = new Array(rows.length);
             var x;
             var j;
             for (j = 0; j < rows.length; j++) {
+                console.log(j);
                 text[j] = '[' + j + '] ';
                 for (x in rows[j]) {
                     text[j] += rows[j][x] + ' ';
